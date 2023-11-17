383d377937d740974aa5f5b1989674fee2df7e6c
vladkharyushin <vx3110@yandex.ru>
2023-11-17 14:16:57 +0300

    First actions

---
+++ src/01/01.test.ts
@@ -0,0 +1,22 @@
+import {mult, splitIntoWords, sum} from './01';
+
+test('sum should be correct', () => {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    const result1 = sum(a,b);
+    const result2 = sum(b,c);
+    expect(result1).toBe(3);
+    expect(result2).toBe(5);
+})
+test('multiply should be correct', () => {
+    const a = 1;
+    const b = 2;
+    const c = 3;
+
+    const result1 = mult(a,b);
+    const result2 = mult(b,c);
+    expect(result1).toBe(2);
+    expect(result2).toBe(6);
+})
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWords} from "./01/01";
+
+const sentense = "Hello my friends";
+const result = splitIntoWords(sentense)
+console.log(result)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +17,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

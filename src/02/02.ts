c2b7cbd9c90e6b6e7fdc6f3773f9c6e85063ed7a
vladkharyushin <vx3110@yandex.ru>
2023-11-18 13:43:10 +0300

    Second actions

---
+++ src/02/02.ts
@@ -0,0 +1,53 @@
+type CityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: CityType
+}
+type TechType = {
+    id: number
+    title: string
+}
+export type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+export const student: StudentType = {
+    id: 1,
+    name: "Vladislav",
+    age: 26,
+    isActive: true,
+    address: {
+        streetTitle: "Lenina 101",
+        city: {
+            title: "Kirov",
+            countryTitle: "Russia",
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+};
+
+console.log(student.age);
+console.log(student.name);
+console.log(student.address.city.title);
+console.log(student.technologies[2].title);
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {splitIntoWords} from "./01/01";
+import {sum} from "./03/03";
 
-const sentense = "Hello my friends";
-const result = splitIntoWords(sentense)
-console.log(result)
+var a = sum;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
